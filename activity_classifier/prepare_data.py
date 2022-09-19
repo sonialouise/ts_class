9020189a131b8e583d36c3220fb9bf9368e3cb95
Sonia Ingram <soniaingram@Sonias-MBP.lan>
2022-09-19 18:33:04 +0100

    Addition of interpolation function

--- activity_classifier/prepare_data.py
+++ activity_classifier/prepare_data.py
@@ -1,21 +1,38 @@
 import pandas as pd
-from activity_classifier.config import OBS
 import numpy as np
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-"""Scaling/normalisation to use as required"""
-scaler = StandardScaler() # MinMaxScaler()
+from activity_classifier.config import OBS
+from scipy.interpolate import interp1d
 
-def normalise_data(data):
+
+def interpolate_data(data, seconds, end_frame_rate):
+    """
+    Calculates given data frame rate and interpolates data to desired end_frame_rate
+    :param data: Recorded data
+    :param seconds: Recorded data total duration in seconds
+    :param end_frame_rate: Desired frame rate
+    :return: Interpolated data per end_frame_rate
+    """
+    recorded_frame_rate = (len(data.columns) - 1) / seconds
+    frame_interval = 1 / end_frame_rate
+    frame_labels = list(range(0, len(data.columns)))
+    x = np.multiply(frame_labels, 1 / recorded_frame_rate)
+    interpolate_func = interp1d(x, data, "linear", fill_value="extrapolate")
+
+    x_new = np.arange(0, seconds + frame_interval, frame_interval)
+    y_new = interpolate_func(x_new)
+    return pd.Series(y_new)
+
+
+def prepare_data(data, seconds, end_frame_rate):
+    """
+    Prepare data for model application, including interpolation.
+    :param data: Recorded data
+    :param seconds: Recorded data total duration in seconds
+    :param end_frame_rate: Desired frame rate
+    :return: Input data with additional column containing interpolated data per end_frame_rate
+    """
     assert data.isnull().sum().sum() == 0, AssertionError("Data contains empty values, correct and retry")
     assert np.isinf(data).values.sum() == 0, AssertionError("Data contains inf values, correct and retry")
-    scaled = scaler.fit_transform(np.array(data))
-    data[OBS] = [pd.Series(row) for row in scaled]
-    return data
-
-
-def prepare_data(data):
-    assert data.isnull().sum().sum() == 0, AssertionError("Data contains empty values, correct and retry")
-    assert np.isinf(data).values.sum() == 0, AssertionError("Data contains inf values, correct and retry")
-    data[OBS] = [pd.Series(row) for row in np.array(data)]
+    data[OBS] = [interpolate_data(row, seconds, end_frame_rate) for row in np.array(data)]
     return data
