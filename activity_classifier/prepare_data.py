ca2f3e960e3cf4fe12bb4c9bc3b22275f2332708
Sonia Ingram <soniaingram@Sonias-MBP.broadband>
2022-10-22 14:37:27 +0100

    reduce output to 550 frames

--- activity_classifier/prepare_data.py
+++ activity_classifier/prepare_data.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 
 from activity_classifier.config import OBS
@@ -13,15 +12,15 @@ def interpolate_data(data, seconds, end_frame_rate):
     :param end_frame_rate: Desired frame rate (frames per second)
     :return: Interpolated data per end_frame_rate
     """
-    recorded_frame_rate = (len(data) - 1) / seconds
+    recorded_frame_rate = len(data) / seconds
     frame_interval = 1 / end_frame_rate
     frame_labels = list(range(0, len(data)))
     x = np.multiply(frame_labels, 1 / recorded_frame_rate)
     interpolate_func = interp1d(x, data, "linear", fill_value="extrapolate")
 
-    x_new = np.arange(0, seconds + frame_interval, frame_interval)
+    x_new = np.arange(0, seconds, frame_interval)
     y_new = interpolate_func(x_new)
-    return pd.Series(y_new)
+    return y_new
 
 
 def prepare_data(data, seconds, end_frame_rate):
@@ -34,5 +33,5 @@ def prepare_data(data, seconds, end_frame_rate):
     """
     assert data.isnull().sum().sum() == 0, AssertionError("Data contains empty values, correct and retry")
     assert np.isinf(data).values.sum() == 0, AssertionError("Data contains inf values, correct and retry")
-    data[OBS] = data.apply(lambda x: [interpolate_data(x, seconds, end_frame_rate)], axis=1)
+    data[OBS] = data.apply(lambda x: interpolate_data(x, seconds, end_frame_rate), axis=1, result_type='reduce')
     return data
