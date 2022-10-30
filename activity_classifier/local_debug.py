0e2de23c5530a03a85c691aa5d9736c09d6f05db
Sonia Ingram <soniaingram@Sonias-MBP.broadband>
2022-10-30 16:12:05 +0000

    update local debug

--- activity_classifier/local_debug.py
+++ activity_classifier/local_debug.py
@@ -1,45 +1,44 @@
 from pathlib import Path
-from config import TSF_MODEL, RISE_MODEL, OBS, PREDICTION, OUTPUT_PATH, LABEL
-from prepare_data import prepare_data
-from main import run_model
-from retrain_models import (
-    retrain_rise,
-    prepare_timeseries_forest_classifier,
-    prepare_random_interval_spectral_ensemble,
-    train_and_cross_validate
-)
+from activity_classifier.config import TSF_MODEL, RISE_MODEL, OUTPUT_PATH
+from activity_classifier.prepare_data import prepare_data
+from activity_classifier.main import run_model
+from retrain_models import retrain_rise, retrain_tsf
 import logging
 import pandas as pd
-import pickle
 
 path = Path('.')
 
 train_data = path.cwd().parent / 'data' / 'training_data.csv'
-test_data = path.cwd().parent / 'data' / 'test_set.csv'
+test_data = path.cwd().parent / 'data' / 'test_data.csv'
+
+duration = 151
+sampling_rate = 3.65
 
 ## PREDICT
+logging.info("1. Reading csv file...")
 data = pd.read_csv(test_data, header=0)
 logging.info("2. Normalising data...")
-data = prepare_data(data.iloc[:, 0:550])
+total_frames = int(duration * sampling_rate)
+trace_data = data.iloc[:, 0:total_frames]
+data = prepare_data(trace_data, duration, sampling_rate)
 logging.info("3. Predicting with TimeSeries Forest Classifier...")
-data = run_model(data, path.cwd().parent / TSF_MODEL, 'TSF')
+data = run_model(data, TSF_MODEL, 'TSF')
 logging.info("4. Predicting with Random Interval Spectral Ensemble...")
-data = run_model(data, path.cwd().parent / RISE_MODEL, 'RISE')
+data = run_model(data, RISE_MODEL, 'RISE')
 logging.info("5. Saving output...")
 data.to_csv(OUTPUT_PATH)
+logging.info("6. Process complete")
 
 
 ## RETRAIN
-# logging.info("1. Reading csv file...")
-# data = pd.read_csv(train_data, header=0)
-# logging.info("2. Normalising data...")
-# data = pd.concat([prepare_data(data.iloc[:, 0:int(550)]), data[LABEL]], axis=1)
-# logging.info("3. Retraining Time Series Classifier...")
-# tsf = prepare_timeseries_forest_classifier()
-# tsf = train_and_cross_validate(data, tsf, 'TSF')
-# pickle.dump(tsf, open(path.cwd().parent / TSF_MODEL, 'wb'))
-# logging.info("4. Retraining Random Interval Spectral Ensemble...")
-# rise = prepare_random_interval_spectral_ensemble()
-# rise = train_and_cross_validate(data, rise, 'RISE')
-# pickle.dump(rise, open(path.cwd().parent / RISE_MODEL, 'wb'))
-# logging.info("5. Retraining Complete")
+logging.info("1. Reading csv file...")
+data = pd.read_csv(train_data, header=0)
+logging.info("2. Normalising data...")
+total_frames = int(duration * sampling_rate)
+trace_data = data.iloc[:, 0:total_frames]
+data = pd.concat([prepare_data(trace_data, duration, sampling_rate), data['status']], axis=1)
+logging.info("3. Retraining Time Series Classifier...")
+retrain_tsf(data)
+logging.info("4. Retraining Random Interval Spectral Ensemble...")
+retrain_rise(data)
+logging.info("5. Retraining Complete")
